2782cf972cdd1a9cde78cbedf9428b1c32e68609
Flo2503 <bruel.florent@gmail.com>
2019-04-02 21:52:38 +0200

    implement ButtonView and clean ControllerView

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -10,16 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-    @IBOutlet weak var firstIconCorrect: UIImageView!
-    @IBOutlet weak var secondIconCorrect: UIImageView!
-    @IBOutlet weak var thirdIconCorrect: UIImageView!
+  @IBOutlet weak var buttonView: ButtonView!
     
-    
-    @IBOutlet weak var firstGridButton: UIButton!
-    @IBOutlet weak var secondGridButton: UIButton!
-    @IBOutlet weak var thirdGridButton: UIButton!
-    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -27,23 +20,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showFirstGrid(_ sender: Any) {
-        thirdIconCorrect.isHidden = true
-        firstIconCorrect.isHidden = false
-        secondIconCorrect.isHidden = true
+        buttonView.style = .firstButton
     }
     
     @IBAction func showSecondGrid(_ sender: Any) {
-        thirdIconCorrect.isHidden = true
-        firstIconCorrect.isHidden = true
-        secondIconCorrect.isHidden = false
+        buttonView.style = .secondButton
     }
     
     
     @IBAction func showThirdGrid(_ sender: Any) {
-        thirdIconCorrect.isHidden = false
-        firstIconCorrect.isHidden = true
-        secondIconCorrect.isHidden = true
-        
+        buttonView.style = .thirdButton
     }
     
 }
--- Instagrid/View/ButtonView.swift
+++ Instagrid/View/ButtonView.swift
@@ -9,7 +9,43 @@
 import UIKit
 
 class ButtonView: UIView {
+
+   
     
+    
+    @IBOutlet weak var firstIconCorrect: UIView!
+    @IBOutlet weak var secondIconCorrect: UIView!
+    @IBOutlet weak var thirdIconCorrect: UIView!
+   
+    
+    enum Style {
+        case firstButton, secondButton, thirdButton
+    }
+    
+    var style: Style = .firstButton {
+        didSet {
+            setStyle(style)
+        }
+    }
+    
+    private func setStyle(_ style: Style) {
+        switch style {
+            case .firstButton:
+                firstIconCorrect.isHidden = false
+                secondIconCorrect.isHidden = true
+                thirdIconCorrect.isHidden = true
+            case .secondButton:
+                firstIconCorrect.isHidden = true
+                secondIconCorrect.isHidden = false
+                thirdIconCorrect.isHidden = true
+            case .thirdButton:
+                firstIconCorrect.isHidden = true
+                secondIconCorrect.isHidden = true
+                thirdIconCorrect.isHidden = false
+        }
+    }
+
+
 }
     
     
