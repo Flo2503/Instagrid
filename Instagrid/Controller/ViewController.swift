0e5dbcc8372f88aefc8ad2a36511628bd3dc1dce
Flo2503 <bruel.florent@gmail.com>
2019-04-10 19:34:41 +0200

    add method fot camera access. create alert to display source choice : camera or library

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
-        NotificationCenter.default.addObserver(self, selector: #selector(selectPicLibrary), name: NSNotification.Name("tapOnGridButtons"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(chooseImage), name: NSNotification.Name("tapOnGridButtons"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(sharePics), name: NSNotification.Name("swipeToShare"), object: nil)
     }
     
@@ -48,19 +48,55 @@ class ViewController: UIViewController {
         secondGrid.isHidden = true
         thirdGrid.isHidden = false
     }
- 
+    
+//
+    @objc func chooseImage() {
+        let alert = UIAlertController(title: "Photo Source", message: "Please choose a source", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { _ in
+            self.cameraAccess()
+        }))
+        alert.addAction(UIAlertAction(title: "Library", style: .default, handler: { _ in
+            self.libraryAccess()
+        }))
+        alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
     
     
 //Access photo library
-    @objc private func selectPicLibrary() {
+    @objc private func libraryAccess() {
         imagePicker.sourceType = .photoLibrary
         imagePicker.allowsEditing = true
         present(imagePicker, animated: true, completion: nil)
     }
+  
+//Acces camera
+    @objc func cameraAccess() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+            imagePicker.allowsEditing = true
+        }else {
+            print("Camera not available")
+        }
+    }
+    
+//Convert UIView to UIImage
+    func convertUIV(with view: UIView) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(view.bounds.size, view.isOpaque, 0.0)
+        defer { UIGraphicsEndImageContext() }
+        if let context = UIGraphicsGetCurrentContext() {
+            view.layer.render(in: context)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            return image
+        }
+        return nil
+    }
+    
+    
     
 //Share photos
     @objc private func sharePics() {
-        if let image = firstGrid.bottomLetftImage.image {
+        if let image = convertUIV(with: firstGrid) {
             let activityVC = UIActivityViewController(activityItems: [image], applicationActivities: nil)
             activityVC.popoverPresentationController?.sourceView = self.view
             
