4c9274e8700494c40334bd567fa78fb1bb3c6bcb
Flo2503 <bruel.florent@gmail.com>
2019-04-22 22:16:42 +0200

    add notifications in Button View. Create class Current View listening notifications and displaying gridss

---
+++ Instagrid/View/CurrentView.swift
@@ -0,0 +1,9 @@
+//
+//  CurrentView.swift
+//  Instagrid
+//
+//  Created by Flo on 22/04/2019.
+//  Copyright © 2019 Flo. All rights reserved.
+//
+
+import Foundation
---
+++ Instagrid/View/GridView.swift
@@ -0,0 +1,9 @@
+//
+//  GridView.swift
+//  Instagrid
+//
+//  Created by Flo on 22/04/2019.
+//  Copyright © 2019 Flo. All rights reserved.
+//
+
+import Foundation
